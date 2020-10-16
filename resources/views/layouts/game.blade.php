19d75526e89a4e937bc5ec913605f0cddae59dd9
Sven Scharfenberg <sscharfenberg@ivugmbh.de>
2020-10-16 17:27:41 +0200

    php dependencies updated

--- resources/views/layouts/game.blade.php
+++ resources/views/layouts/game.blade.php
@@ -5,21 +5,28 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1.0" />
         <meta name="description" content="Imperium Stellarum - a turn-based multiplayer browser game of galactic conquest." />
-        
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicons/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicons/favicon-16x16.png">
+        <link rel="manifest" href="/assets/favicons/site.webmanifest">
+        <link rel="mask-icon" href="/assets/favicons/safari-pinned-tab.svg" color="#6fc3df">
+        <link rel="shortcut icon" href="/assets/favicons/favicon.ico">
         <meta name="apple-mobile-web-app-title" content="Imperium Stellarum">
         <meta name="application-name" content="Imperium Stellarum">
         <meta name="msapplication-TileColor" content="#12141c">
-        
+        <meta name="msapplication-config" content="/assets/favicons/browserconfig.xml">
         <meta name="theme-color" content="#12141c">
+        
+            <link rel="stylesheet" type="text/css" href="/assets/app.f2ece42e32138a72.css" />
+        
+            <link rel="stylesheet" type="text/css" href="/assets/admin.c3398431e3244d13.css" />
         
     </head>
     <body>
-        <script type="text/javascript" src="http://localhost:7777/chunk-vendors.js" defer></script>
-        <script type="text/javascript" src="http://localhost:7777/app.js" defer></script>
-        <script type="text/javascript" src="http://localhost:7777/game.js" defer></script>
-        <script type="text/javascript" src="http://localhost:7777/admin.js" defer></script>
-        
-        <script type="text/javascript" src="http://localhost:7777/webpack-dev-server.js" defer></script>
+        0<script type="text/javascript" src="/assets/chunk-vendors.7e677a910272047b.js" defer></script>
+        1<script type="text/javascript" src="/assets/app.f2ece42e32138a72.js" defer></script>
+        2<script type="text/javascript" src="/assets/game.fa138d8827455182.js" defer></script>
+        3<script type="text/javascript" src="/assets/admin.c3398431e3244d13.js" defer></script>
         
     </body>
 </html>
