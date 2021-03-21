5fe6caa6b446f8d03734dc7638e975b52e59c1b9
Sven Scharfenberg <sscharfenberg@ivugmbh.de>
2021-03-21 16:06:14 +0100

    refactored collection model for encounters, removed "defenderShips" and "attackerShips", they are now placed below "attacker/defender" array with a seperate object for each entity (fleet/shipyard). added prefered fleet range to model by calculating which weapon does the most damage (hullType, moduleType).

--- app/Actions/Turn/Encounter/FindEncounters.php
+++ app/Actions/Turn/Encounter/FindEncounters.php
@@ -2,16 +2,20 @@
 
 namespace App\Actions\Turn\Encounter;
 
-use App\Models\Player;
+use App\Models\Fleet;
 use App\Models\PlayerRelation;
 use App\Models\Ship;
 use App\Models\Shipyard;
 use App\Models\Star;
-use App\Services\PlayerRelationService;
+use App\Models\Game;
+
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Log;
-use App\Models\Game;
 use Exception;
+use PhpParser\Node\Expr\Array_;
+use Ramsey\Uuid\Uuid;
+
+use App\Services\PlayerRelationService;
 
 class FindEncounters
 {
@@ -20,36 +24,109 @@ class FindEncounters
      * @function default encounter model
      * @param Star $star
      * @param Collection $shipyards
+     * @param int $game
+     * @param int $turn
      * @return Collection
      */
-    private function createEncounter (Star $star, Collection $shipyards): Collection
+    private function createEncounter (Star $star, Collection $shipyards, int $game, int $turn): Collection
     {
         return collect([
-            'star' => $star,
+            'id' => Uuid::uuid4(),
+            'game' => $game,
+            'turn' => $turn,
+            'game_id' => $star->game_id,
+            'star' => [
+                'id' => $star->id,
+                'name' => $star->name,
+                'spectral' => $star->spectral,
+                'owner' => $star->owner
+            ],
             'defender' => collect(),
-            'defenderShips' => collect(),
             'shipyards' => $shipyards,
             'attacker' => collect(),
-            'attackerShips' => collect()
         ]);
     }
 
     /**
-     * @function add ships to shipyard/fleet
-     * @param Collection $holder
-     * @param Collection $ships
-     * @param string $collectionId
-     * @return Collection
+     * @function static damage ranges setup
+     * @return array
      */
-    private function addShips (Collection $holder, Collection $ships, string $collectionId): Collection
+    private function damageRanges (): array
     {
-        return $holder->concat(
-            $ships->filter(function($ship) use ($collectionId){
-                return $ship->fleet_id === $collectionId || $ship->shipyard_id === $collectionId;
-            })
-        );
+        return [
+            'small.laser' => 0,
+            'small.missile' => 0,
+            'small.railgun' => 0,
+            'small.plasma' => 0,
+            'medium.laser' => 0,
+            'medium.missile' => 0,
+            'medium.railgun' => 0,
+            'medium.plasma' => 0,
+            'large.laser' => 0,
+            'large.missile' => 0,
+            'large.railgun' => 0,
+            'large.plasma' => 0,
+            'xlarge.laser' => 0,
+            'xlarge.missile' => 0,
+            'xlarge.railgun' => 0,
+            'xlarge.plasma' => 0,
+        ];
     }
 
+    /**
+     * @function format a fleet for data handling
+     * @param Fleet $fleet
+     * @param array $ships
+     * @param int $col
+     * @return array
+     */
+    private function formatFleet (Fleet $fleet, array $ships, int $col): array
+    {
+        $dmg = $this->damageRanges();
+        $preferedRange = 10;
+        foreach($ships as $ship) {
+            $hullType = $ship['hull_type'];
+            if ($ship['dmg_laser'] > 0) $dmg[$hullType.".laser"] += $ship['dmg_laser'];
+            if ($ship['dmg_missile'] > 0) $dmg[$hullType.".missile"] += $ship['dmg_missile'];
+            if ($ship['dmg_railgun'] > 0) $dmg[$hullType.".railgun"] += $ship['dmg_railgun'];
+            if ($ship['dmg_plasma'] > 0) $dmg[$hullType.".plasma"] += $ship['dmg_plasma'];
+        }
+        $maxDmg = max($dmg);
+        if ($maxDmg > 0) {
+            $maxDmgWpnType = array_search(max($dmg), $dmg);
+            $preferedRange = collect(config('rules.modules'))->filter(function($mod) use ($maxDmgWpnType) {
+                return $mod['hullType'] === explode('.', $maxDmgWpnType)[0]
+                    && $mod['techType'] === explode('.', $maxDmgWpnType)[1];
+            })->first()['range'];
+        }
+        return [
+            'id' => $fleet->id,
+            'player_id' => $fleet->player_id,
+            'name' => '['.$fleet->player->ticker.'] '.$fleet->name,
+            'col' => $col,
+            'prefered_range' => $preferedRange,
+            'ships' => collect($ships)
+        ];
+    }
+
+    /**
+     * @function format a fleet for data handling
+     * @param Shipyard $shipyard
+     * @param array $ships
+     * @param int $col
+     * @return array
+     */
+    private function formatShipyard (Shipyard $shipyard, array $ships, int $col): array
+    {
+        return [
+            'id' => $shipyard->id,
+            'player_id' => $shipyard->player_id,
+            'starName' => $shipyard->planet->star->name,
+            'planetOrbitalIndex' => $shipyard->planet->orbital_index,
+            'col' => $col,
+            'ships' => collect($ships)
+        ];
+    }
 
     /**
      * @function get the encounters for a star
@@ -57,9 +134,18 @@ class FindEncounters
      * @param Collection $gameRelations
      * @param Collection $shipyards
      * @param string $turnSlug
+     * @param int $game
+     * @param int $turn
      * @return Collection
      */
-    private function getStarEncounter (Star $star, Collection $gameRelations, Collection $shipyards, string $turnSlug): Collection
+    private function getStarEncounter (
+        Star $star,
+        Collection $gameRelations,
+        Collection $shipyards,
+        string $turnSlug,
+        int $game,
+        int $turn
+    ): Collection
     {
         $p = new PlayerRelationService;
 
@@ -74,13 +160,7 @@ class FindEncounters
                 "$turnSlug found ".count($star->fleets)
                 ." fleets and ".count($starShipyards)." shipyards in system."
             );
-        $encounter = $this->createEncounter($star, $starShipyards);
-        $fleetIds = $star->fleets->map(function ($fleet) {
-            return $fleet->id;
-        });
-        $fleetShips = Ship::where('game_id', '=', $star->game_id)
-            ->whereIn('fleet_id', $fleetIds)
-            ->get();
+        $encounter = $this->createEncounter($star, $starShipyards, $game, $turn);
 
         // assign fleet ships to attacker/defender
         foreach($star->fleets as $fleet) {
@@ -88,39 +168,29 @@ class FindEncounters
             if ($star->owner->id !== $fleet->player_id) {
                 $effectiveRelation = $p->getEffectiveRelation($fleet->player, $star->owner, $gameRelations);
                 // hostile fleet, add to attacker collection
-                if ($effectiveRelation === 0) {
+                if ($effectiveRelation === 0 && count($fleet->ships) > 0) {
                     Log::channel('turn')
-                        ->notice("$turnSlug found hostile fleet [".$fleet->player->ticker."] $fleet->name.");
-                    $encounter['attacker']->push($fleet);
-                    $encounter['attackerShips'] = $this->addShips($encounter['attackerShips'], $fleetShips, $fleet->id);
+                        ->notice("$turnSlug found hostile fleet [".$fleet->player->ticker."] $fleet->name with ".count($fleet->ships)." ships.");
+                    $encounter['attacker']->push($this->formatFleet($fleet, $fleet->ships->toArray(), 10));
                 }
                 // allied fleet, add to defender collection
-                if ($effectiveRelation === 2) {
+                if ($effectiveRelation === 2 && count($fleet->ships) > 0) {
                     Log::channel('turn')
-                        ->notice("$turnSlug found allied fleet [".$fleet->player->ticker."] $fleet->name.");
-                    $encounter['defender']->push($fleet);
-                    $encounter['defenderShips'] = $this->addShips($encounter['defenderShips'], $fleetShips, $fleet->id);
+                        ->notice("$turnSlug found allied fleet [".$fleet->player->ticker."] $fleet->name with ".count($fleet->ships)." ships.");
+                    $encounter['defender']->push($this->formatFleet($fleet, $fleet->ships->toArray(), 0));
                 }
-            } else {
+            } else if (count($fleet->ships) > 0) {
                 // star owner fleet, add to defender
                 Log::channel('turn')
-                    ->notice("$turnSlug found fleet of star owner [".$fleet->player->ticker."] $fleet->name.");
-                $encounter['defender']->push($fleet);
-                $encounter['defenderShips'] = $this->addShips($encounter['defenderShips'], $fleetShips, $fleet->id);
+                    ->notice("$turnSlug found fleet of star owner [".$fleet->player->ticker."] $fleet->name with ".count($fleet->ships)." ships.");
+                $encounter['defender']->push($this->formatFleet($fleet, $fleet->ships->toArray(), 0));
             }
         }
 
         // assign shipyard ships to defender
-        $shipyardIds = $starShipyards->map(function($shipyard) {
-            return $shipyard->id;
-        });
-        $shipyardShips = Ship::where('game_id', '=', $star->game_id)
-            ->whereIn('shipyard_id', $shipyardIds)
-            ->get();
-        if (count($starShipyards) > 0) {
-            foreach ($starShipyards as $shipyard) {
-                $encounter['defenderShips'] = $this->addShips($encounter['defenderShips'], $shipyardShips, $shipyard->id);
-            }
+        foreach ($starShipyards as $shipyard) {
+            if (count($shipyard->ships) > 0)
+            $encounter['defender']->push($this->formatShipyard($shipyard, $shipyard->ships->toArray(), 0));
         }
 
         return $encounter;
@@ -151,7 +221,11 @@ class FindEncounters
             });
         $shipyards = Shipyard::where('game_id', $game->id)
             ->where('until_complete', '=', 0)
+            ->with('ships')
             ->get();
+        $turn = $game->turns
+            ->whereNull('processed')
+            ->first();
 
         Log::channel('turn')
             ->notice($turnSlug.' - found '.count($stars).' stars that have a potential encounter.');
@@ -159,12 +233,12 @@ class FindEncounters
         // main loop - check each star and see if there is an encounter.
         foreach($stars as $star) {
             // create encounter with all necessary data
-            $encounter = $this->getStarEncounter($star, $gameRelations, $shipyards, $turnSlug);
+            $encounter = $this->getStarEncounter($star, $gameRelations, $shipyards, $turnSlug, $game->number, $turn->number);
             // if there are attacking ships, handle encounter processing
             if (count($encounter['attacker']) > 0) {
                 Log::channel('turn')
-                    ->notice("$turnSlug found valid encounter @ [".$star->owner->ticker."] $star->name.");
-                $p->handle($game, $encounter, $turnSlug);
+                    ->notice("$turnSlug found valid encounter ".$encounter['id']." @ [".$star->owner->ticker."] $star->name.");
+                $p->handle($encounter, $turnSlug);
             } else {
                 Log::channel('turn')
                     ->notice("$turnSlug no encounter @ [".$star->owner->ticker."] $star->name.");
--- app/Actions/Turn/Encounter/ProcessEncounter.php
+++ app/Actions/Turn/Encounter/ProcessEncounter.php
@@ -11,27 +11,74 @@ class ProcessEncounter
 {
 
     /**
+     * @function move fleets (changing row)
+     * @param Collection $encounter
+     * @param string $turnSlug
+     * @param int $turn
+     */
+    private function moveFleets (Collection $encounter, string $turnSlug, int $turn)
+    {
+        Log::channel('encounter')
+            ->info("$turnSlug #".$encounter['id']." turn $turn STEP 1: MOVE FLEETS.");
+
+        //foreach($encounter['attacker'] as $fleet) {
+//
+        //    dd($fleet['ships']);
+        //}
+        // find out which distance the fleet wants to be at
+        // move as far as possible to this column
+    }
+
+
+    private function processEncounter (Collection $encounter, string $turnSlug)
+    {
+        $resolved = false;
+        $turn = 0;
+        while(!$resolved) {
+            $turn++;
+            Log::channel('encounter')
+                ->notice("#".$encounter['id']." start processing turn $turn.");
+            echo "encounter turn $turn\n";
+            // 1) Move Fleets
+            $this->moveFleets($encounter, $turnSlug, $turn);
+            // 2) Select targets for ships
+            // 3) Assign damage to targets
+            // 4) Check for destroyed ships and remove them
+            // 5) check if encounter ends (no attacker or defender ships)
+            // 6) increase turn
+
+
+
+            // temp so it is not never-ending.
+            if ($turn >= 10) $resolved = true;
+        }
+    }
+
+
+    /**
      * @function find out if any encounters need to be processed and trigger processing.
-     * @param Game $game
      * @param string $turnSlug
      * @param Collection $encounter
      * @throws Exception
      * @return void
      */
-    public function handle(Game $game, Collection $encounter, string $turnSlug)
+    public function handle(Collection $encounter, string $turnSlug)
     {
-        echo "$turnSlug processing encounter at star ".$encounter['star']['name']."\n";
+        // tons of logging.
         Log::channel('encounter')
-            ->debug(
-                "start processing of encounter at ["
-                .$encounter['star']->owner->ticker."] "
-                .$encounter['star']['name'].".\n"
-                .$encounter->toJson(JSON_PRETTY_PRINT)
+            ->notice(
+                "$turnSlug start processing of #".$encounter['id']." at ["
+                .$encounter['star']['owner']->ticker."] "
+                .$encounter['star']['name']
             );
-        foreach($encounter['defenderShips'] as $ship) {
-            echo "$ship->name\n";
-        }
-        dd(count($encounter['defenderShips']));
+        Log::channel('encounter')
+            ->debug("#".$encounter['id']." star: ".json_encode($encounter['star'], JSON_PRETTY_PRINT));
+        Log::channel('encounter')
+            ->debug("#".$encounter['id']." attacker: ".json_encode($encounter['attacker'], JSON_PRETTY_PRINT));
+        Log::channel('encounter')
+            ->debug("#".$encounter['id']." defender: ".json_encode($encounter['defender'], JSON_PRETTY_PRINT));
+        // start processing.
+        $this->processEncounter($encounter, $turnSlug);
     }
 
 }
